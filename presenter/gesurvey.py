8369a481f96697cadb58ac2125f6feabc978e88c
Stefan Lemme <stefan.lemme@dfki.de>
2014-05-28 10:30:21 +0200

    add presenter for GE survey recipients

---
+++ presenter/gesurvey.py
@@ -0,0 +1,50 @@
+
+from presenter import PresenterBase
+from visitor import GEVisitor, UsedByVisitor
+
+
+class GESurveyPresenter(PresenterBase):
+	def __init__(self, hideunused = False):
+		PresenterBase.__init__(self)
+		self.v = GEVisitor()
+
+	def present(self, meta):
+		self.v.visit(meta)
+		self.validation = {}
+		
+		for ge in self.v.result:
+			uv = UsedByVisitor(ge, ['USES', 'WILL USE', 'MAY USE'], se=True, app=True, experiment=False, transitive=False)
+			uv.visit(meta)
+			
+			uve = set(uv.result)
+			
+			ses = [e for e in uve if e.entity == 'SE']
+			apps = [e for e in uve if e.entity == 'APP']
+
+			
+			for se in ses:
+				partner, contact = se.provider
+				
+				if not partner in self.validation:
+					self.validation[partner] = {"contacts" : set(), "GEs" : set()}
+					
+				self.validation[partner]["contacts"].add(contact)
+				self.validation[partner]["GEs"].add(ge)
+
+			for app in apps:
+				partner, contact = app.developer
+				
+				if not partner in self.validation:
+					self.validation[partner] = {"contacts" : set(), "GEs" : set()}
+					
+				self.validation[partner]["contacts"].add(contact)
+				self.validation[partner]["GEs"].add(ge)
+
+				
+	def dump(self, out):
+		out.write('^ Partner  ^ Contacts  ^ GEs  ^')
+		for partner, val in self.validation.items():
+			contacts = list(val["contacts"])
+			ges = [ge.identifier for ge in val["GEs"]]
+			out.write('| %s    |  %s  | %s       |' % (partner.identifier, " \\\\ ".join(contacts), " \\\\ ".join(ges)))
+		# out.write('| ...   | ...   | ...      |')
